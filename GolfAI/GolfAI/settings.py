203af26859a6b37ffdbb25051835e554efa009b5
dorstrauss <97314875+dorstrauss@users.noreply.github.com>
2023-02-20 15:32:18 +0200

    Change the database of the project to postgresql, and created a requirements file.

--- GolfAI/GolfAI/settings.py
+++ GolfAI/GolfAI/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'GolfAI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'golf-analytics-database',
+        'USER': 'golfuser',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': '5434'
+
     }
 }
 
